7b70cb09184852c25b7ab0032a4a310a5264d1df
Meissshu <allayarov.marsel4305@mail.ru>
2017-05-29 12:34:40 +0300

    Data in list now refreshes after changes

--- app/src/main/java/com/meishu/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/meishu/android/criminalintent/CrimeListFragment.java
@@ -37,8 +37,12 @@ public class CrimeListFragment extends Fragment {
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        adapter = new CrimeAdapter(crimes);
-        crimeRecyclerView.setAdapter(adapter);
+        if (adapter == null) {
+            adapter = new CrimeAdapter(crimes);
+            crimeRecyclerView.setAdapter(adapter);
+        } else {
+            adapter.notifyDataSetChanged();
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -94,5 +98,11 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return crimes.size();
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
     }
 }
