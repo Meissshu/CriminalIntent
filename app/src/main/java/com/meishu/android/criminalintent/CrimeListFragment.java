9a4dea50a799257c7086f608efbbf759daaa402d
Meissshu <allayarov.marsel4305@mail.ru>
2017-05-29 11:45:03 +0300

    CrimeListFragment Test branch creation, open CrimeActivity from fragment

--- app/src/main/java/com/meishu/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/meishu/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.meishu.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -64,7 +64,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), crime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
