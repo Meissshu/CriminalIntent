9a79173b7730227110c02cd0380e9707dab7d0eb
Meissshu <allayarov.marsel4305@mail.ru>
2017-07-16 14:40:52 +0300

    Add callbacks to make HOST activity work with filling fragments by replacing and committing

--- app/src/main/java/com/meishu/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/meishu/android/criminalintent/CrimeListActivity.java
@@ -1,12 +1,13 @@
 package com.meishu.android.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by Meishu on 17.03.2017.
  */
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
@@ -15,5 +16,18 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+        if (findViewById(R.id.detail_fragmentContainer) == null) {
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+            startActivity(intent);
+        } else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragmentContainer, newDetail)
+                    .commit();
+        }
     }
 }
